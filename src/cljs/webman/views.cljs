539d386187e18a405e8c5a96eb1efaaae33450af
amirrezaask <raskarpour@gmail.com>
2017-10-11 00:29:06 +0330

    added footer and post section and logo

--- src/cljs/webman/views.cljs
+++ src/cljs/webman/views.cljs
@@ -7,7 +7,14 @@
   (home/home-panel))
 (defn navbar []
   (home/navbar))
-  
+(defn footer []
+  (home/footer)
+  )
+(defn posts []
+  ;should get new posts about clojure from dev heroes
+
+  (home/Posts [{:link "google.com" :title "TEST" :summary "test"}])
+)
 (defn- panels [panel-name]
   (case panel-name
     :home-panel [home-panel]
--- src/cljs/webman/views/home.cljs
+++ src/cljs/webman/views/home.cljs
@@ -18,14 +18,64 @@
         [:nav {:class "kl-navbar"} 
           [:ul {:class "kl-nav"}
             [:li 
-              [:a {:href "http://google.com"} "صفحه اصلی"]
+              [:a {:href ""} "صفحه اصلی"]
             ]
             [:li 
-              [:a {:href "http://google.com"} "رویداد ها"]
+              [:a {:href ""} "درباره ما"]
+            ]
+            [:li
+              [:a {:href ""} "تماس با ما"]
+            ]
+            [:li
+              [:a {} "مقالات"]
+            ]
+            [:li 
+              [:a {} "میتینگ ها"]
             ]   
+          ]
+          [:div {:class "logo"}
+            [:h2 {} "Clojure UserGroup"]
+            [:img {:src "images/fav.svg" :width "80px" :height "80px"}]
           ]
         ]
       ]
     ]
   ]
+)
+;posts will be vector of maps
+(defn Posts [posts] 
+  [:main 
+    [:div {:class "container"}
+      [:article {:class "post-content"} 
+        [:ul {:class "list-post"}    
+          (for [post posts]  
+            [:li {:class "post"}
+              [:a {:class "post" :href (:link post)} (:title post)]
+              [:p (:summary post)]
+            ]
+          )  
+        ]
+      ]
+    ]
+  ]
+)
+
+(defn footer []
+  [:footer {}
+    [:section {:class "kl-sponsers"}
+      [:h4 "حامیان ما"]
+      [:ul {:class "sponsers"}
+        [:li 
+          [:a {:href ""} 
+            [:img {:src "/images/me-1.jpg"}]
+          ]
+        ]
+        [:li 
+          [:a {:href ""} 
+            [:img {:src "/images/me-1.jpg"}]
+          ]
+        ]
+      ]
+    ]
+  ]  
 )
