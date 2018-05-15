b4e0eb78348adcc931209c9dd54880620574010e
Sameer Rahmani <lxsameer@gnu.org>
2018-05-15 20:53:14 +0100

    navbar has been improved

--- src/cljs/webman/views/nav.cljs
+++ src/cljs/webman/views/nav.cljs
@@ -4,22 +4,45 @@
   (:require-macros
    [webman.website :refer [get-config]]))
 
-;; TODO: replace menu with a build time configuration.
 
-(def menu-items
-  [{:name (t [:home_page "Home"]) :href "#"}])
+(def left-side-menu-items
+  "Returns a vector of hashmaps describing menu entries
+  of the left side of navigation menu"
+  (:left (get-config :navbar-menu)))
+
+(def right-side-menu-items
+  "Returns a vector of hashmaps describing menu entries
+  of the left side of navigation menu"
+  (:right (get-config :navbar-menu)))
+
 
 (defn bar
+  "Top navigation menu."
   []
   [:nav {:className "navbar is-success is-fixed-top"}
    [:div {:className :navbar-brand}
     [:a {:href "#"
          :className "navbar-item"}
      (get-config :title)]]
+
    [:div {:className "navbar-menu"}
     [:div {:className "navbar-end"}
-     (for [item menu-items]
+
+     (for [item left-side-menu-items]
        ^{:key item}
-       [:a {:className "navbar-item"
-            :href (:href item)} (:name item)])]
-    [:div {:className "navbar-start"}]]])
+       [:a {:className "navbar-item"}
+        [:a {:className (:class item)
+             :href      (:href item)}]
+        (when (:icon item)
+          [:i {:className (:icon item)}])
+        (t (:name item))])]
+
+    [:div {:className "navbar-start"}
+     (for [item right-side-menu-items]
+       ^{:key item}
+       [:div {:className   "navbar-item"}
+        [:a {:className  (str "navbar-item " (:class item))
+             :href       (:href item)}
+         (when (:icon item)
+           [:i {:className (:icon item)}])
+         (t (:name item))]])]]])
