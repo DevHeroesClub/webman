b90627f5378b841edd2559fd1fe7625bb142fe5b
Sameer Rahmani <lxsameer@gnu.org>
2018-05-22 23:24:28 +0100

    build configuration has been completed. asset edn tag has been added

--- src/clj/webman/website.clj
+++ src/clj/webman/website.clj
@@ -2,18 +2,30 @@
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
+(defn asset
+  "Reader function for `#asset` edn tag which append a random querystring
+  to the assets url."
+  [url]
+  (format "%s?%s" url (int (* 100000000 (rand)))))
+
+(def readers
+  {'asset asset})
+
 (defn website-name
   ([] (website-name "default"))
   ([default]
    (or (System/getenv "WEBMAN_WEBSITE") default)))
 
+
 (defmacro get-config
   [key]
   (let [config-name (format "websites/%s.edn" (website-name))
+        resource    (io/resource config-name)
+        defaults    (edn/read-string {:readers readers}
+                                     (slurp (io/resource "websites/default.edn")))
+        config      (merge defaults (edn/read-string {:readers readers}
+                                                     (slurp resource)))]
 
-
-        resource (io/resource config-name)
-        config (edn/read-string (slurp resource))]
     (get config key)))
 
 (defmacro require-index
